2e32edc26e628af9531a5dedcb7b55185467b76f
David Chart <abibugs@davidchart.com>
2002-03-26 18:10:16 +0000

    Cal Arndt's scripting work and bug fixes.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword-docs/trunk@7245 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- make-html.sh
+++ make-html.sh
@@ -1,26 +1,58 @@
-#! /bin/bash
-CVS=/home/cal/abiword/abiword-docs
+#! /bin/sh
 
-for i in $(ls -1 *.abw);do abiword -to xhtml $i; done
+#  The AbiWord Document Merger
+#
+#  Copyright (C) 2002 Free Software Foundation.
+#
+#  make-html.sh is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
+#
+#  make-html.sh is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
 
-for i in $(ls -1 *.info);
- do $CVS/make-abidoc.pl -I $i -S header.xhtml -F footer.xhtml>`n=$i && echo $n|cut -f1 -d .`.html;
- done
+CVS=`pwd -P`
 
-echo "DONE WITH TOP...#######################################"
+export ABISUITE_HOME=/usr/local/share/AbiSuite
 
-# for dir in how-to info interface problems tutorial;
-for dir in tutorial info problems ;
+
+for dir in ./ howto info interface problems tutorial
+
 do
-cd $dir
-echo `pwd`
-    for i in $(ls -1 *.abw);
+cd ABW/en-US/$dir
+
+  for i in $(ls -1 *.abw)
     do
-     abiword -to xhtml `basename $i`;
+      n=`echo $i|cut -f1 -d .`
+      echo $i
+      abiword -to xhtml `basename $i`
+      $CVS/make-abidoc.pl -I $n.info -S header.xhtml -F footer.xhtml>$n.html
+      rm -f $n.xhtml
     done
 
-    for i in $(ls -1 *.info);
-     do $CVS/make-abidoc.pl -I $i -S header.xhtml -F footer.xhtml>`n=$i && echo $n|cut -f1 -d .`.html;
-     done
-cd ..;
+cd $CVS
+
+done
+
+
+cd $CVS/ABW/en-US
+rm -rf $CVS/help/en-US
+mkdir $CVS/help/en-US
+cp -r */ $CVS/help/en-US
+cp *.html $CVS/help/en-US
+cp *.css $CVS/help/en-US
+
+for i in $(ls -1d $CVS/help/en-US/*/ )
+do
+rm -f $i/*.xhtml
+rm -f $i/*.info
+rm -f $i/*.abw
 done
