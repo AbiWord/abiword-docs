75892ae530b1777339a25c2e74e572acd721c890
Hubert Figuiere <hub@figuiere.net>
2002-06-12 12:07:29 +0000

    Re-indent create help dir if needed. change abiword call due to command line syntax change
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword-docs/trunk@8290 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- make-html.sh
+++ make-html.sh
@@ -26,34 +26,36 @@ export ABISUITE_HOME=/usr/local/share/AbiSuite
 for help_language in en-US fr-FR
 do
 
-for dir in ./ howto info interface problems tutorial plugins
-do
-cd ABW/$help_language/$dir
-  for i in $(ls -1 *.abw)
-    do
-      n=`echo $i|cut -f1 -d .`
-      echo $i
-      abiword -to xhtml `basename $i`
-      $CVS/make-abidoc.pl -I $n.info -S header.xhtml -F footer.xhtml>$n.html
-      rm -f $n.xhtml
-    done
+	for dir in ./ howto info interface problems tutorial plugins
+	do
+		cd ABW/$help_language/$dir
+		for i in $(ls -1 *.abw)
+		do
+		      n=`echo $i|cut -f1 -d .`
+		      echo $i
+		      abiword --to=xhtml `basename $i`
+		      $CVS/make-abidoc.pl -I $n.info -S header.xhtml -F footer.xhtml>$n.html
+		      rm -f $n.xhtml
+		done
 
-cd $CVS
+		cd $CVS
+	done
+
+	cd $CVS/ABW/$help_language
+	rm -rf $CVS/help/$help_language
+	mkdir -p $CVS/help/$help_language
+	cp -r */ $CVS/help/$help_language
+	cp *.html $CVS/help/$help_language
+	cp *.css $CVS/help/$help_language
+	find $CVS/help -name CVS -exec rm -fr {} \;
+
+	for i in $(ls -1d $CVS/help/$help_language/*/ )
+	do
+		rm -f $i/*.xhtml
+		rm -f $i/*.info
+		rm -f $i/*.abw
+	done
+
+	cd $CVS
 done
 
-cd $CVS/ABW/$help_language
-rm -rf $CVS/help/$help_language
-mkdir $CVS/help/$help_language
-cp -r */ $CVS/help/$help_language
-cp *.html $CVS/help/$help_language
-cp *.css $CVS/help/$help_language
-
-for i in $(ls -1d $CVS/help/$help_language/*/ )
-do
-rm -f $i/*.xhtml
-rm -f $i/*.info
-rm -f $i/*.abw
-done
-
-cd $CVS
-done
