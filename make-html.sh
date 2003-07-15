15e0edaaef50769b8fec2a2d4705986df23b8178
Mark Gilbert <mgilbert@svn.abisource.com>
2003-07-15 17:14:27 +0000

    1) make-html now lets you output to destination of choice, source from    location of choice, merge with program of choice, merge from place of    choice, and so forth.  Controlled using environmental variables    ABI_DOC_{SOURCE,PROG,DEST} and everything defaults to the way it worked    before if these aren't set. 2) howtomailmerge cleaned up (line breaks instead of paragraph breaks in    the examples) and slightly reworded.  Linked up in preparation for some    other docs I hope to add.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword-docs/trunk@11904 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- make-html.sh
+++ make-html.sh
@@ -19,43 +19,47 @@
 #  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #
 
-CVS=`pwd -P`
-
-abiword_exec=$1
-
-if [ -z "$abiword_exec" ]
-then
-	abiword_exec=AbiWord-2.0
+if [[ -n $ABI_DOC_SOURCE ]]; then
+CVS="$ABI_DOC_SOURCE";
+else
+CVS=`pwd -P`;
 fi
 
+if [[ -z $ABI_DOC_DEST ]]; then
+ABI_DOC_DEST="$CVS";
+fi
+
+if [[ -z $ABI_DOC_PROG ]]; then
+ABI_DOC_PROG="AbiWord-2.0";
+fi
 
 for help_language in en-US fr-FR pl-PL #de-DE
 do
 
 	for dir in ./ howto info interface problems tutorial plugins
 	do
-		cd ABW/$help_language/$dir
+		cd $CVS/ABW/$help_language/$dir
 		for i in $(ls -1 *.abw)
 		do
 		      n=`echo $i|cut -f1 -d .`
 		      echo $i
-		      $abiword_exec --to=xhtml `basename $i` 2>/dev/null
+		      $ABI_DOC_PROG --to=xhtml `basename $i` 2>/dev/null
 		      $CVS/make-abidoc.pl -I $n.info -S header.xhtml -F footer.xhtml>$n.html
 		      rm -f $n.xhtml
 		done
 
 		cd $CVS
 	done
-
+	
 	cd $CVS/ABW/$help_language
-	rm -rf $CVS/help/$help_language
-	mkdir -p $CVS/help/$help_language
-	cp -r */ $CVS/help/$help_language
-	cp *.html $CVS/help/$help_language
-	cp *.css $CVS/help/$help_language
-	find $CVS/help -name CVS -exec rm -fr {} \;
+	rm -rf $ABI_DOC_DEST/help/$help_language
+	mkdir -p $ABI_DOC_DEST/help/$help_language
+	cp -r */ $ABI_DOC_DEST/help/$help_language
+	cp *.html $ABI_DOC_DEST/help/$help_language
+	cp *.css $ABI_DOC_DEST/help/$help_language
+	find $ABI_DOC_DEST/help -name CVS -exec rm -fr {} \;
 
-	for i in $(ls -1d $CVS/help/$help_language/*/ )
+	for i in $(ls -1d $ABI_DOC_DEST/help/$help_language/*/ )
 	do
 		rm -f $i/*.xhtml
 		rm -f $i/*.info
@@ -64,4 +68,3 @@ do
 
 	cd $CVS
 done
-
