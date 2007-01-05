3d338a8358e8d1efa0044fbe61a53528e31a8e6b
Rob Staudinger <robert.staudinger@gmail.com>
2007-01-05 11:28:23 +0000

    auto build system for abiword-docs, featuring 'make dist'
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword-docs/trunk@19548 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ make-html.mk
@@ -0,0 +1,25 @@
+
+docsrc := $(wildcard $(srcdir)/*.abw)
+doctrg_ := $(patsubst $(srcdir)/%, ./%, $(docsrc))
+doctrg := $(doctrg_:.abw=.html)
+
+# $(helpdir) must be in where this file is included
+help_DATA = \
+	$(doctrg)
+
+EXTRA_DIST = \
+	$(docsrc) \
+	$(wildcard $(srcdir)/*.info) \
+	header.xhtml \
+	footer.xhtml
+
+CLEANFILES = \
+	$(doctrg)
+
+html_export_options = "html4: no; use-awml: no; embed-css: yes; embed-images:yes"
+
+%.html: %.abw
+	abiword --to=html --to-name="$@" --exp-props=$(html_export_options) "$^"
+	info=`echo "$^" | sed -e "s|.abw|.info|g"` && \
+	$(top_srcdir)/make-abidoc.pl -B "$@" -I "$$info" -S $(srcdir)/header.xhtml -F $(srcdir)/footer.xhtml > "$@.tmp" && \
+	mv "$@.tmp" "$@"
